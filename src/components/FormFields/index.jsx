70692f24b49bea8228ee635ffadcf6b8ebe98623
Cancellier27 <filipecancelliercosta@gmail.com>
2020-08-12 23:16:15 +0100

    finishing day 3

--- src/components/FormFields/index.jsx
+++ src/components/FormFields/index.jsx
@@ -1,33 +1,83 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background: #53585D;
+  color: #f5f5f5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+
+  padding: 16px;
+  margin-bottom: 4px;
+
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+
+  &:focus {
+    border-bottom-color: var(--primary)
+  }
+`;
+
+const Label = styled.label``;
+Label.Text = styled.span``;
+
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
 
 function FormField({
   label, type, name, value, onChange,
 }) {
   const fieldId = `id_${name}`;
+  const isTextarea = type === 'textarea';
+  const tag = isTextarea ? 'textarea' : 'input';
 
   return (
-    <div>
-      <label
+    <FormFieldWrapper>
+      <Label
         htmlFor={fieldId}
       >
-        {label}
-        <input
+        <Label.Text>
+
+          {label}
+
+        </Label.Text>
+
+        <Input
+          as={tag}
           type={type}
           name={name}
           value={value}
           onChange={onChange}
+          placeholder={label}
         />
-      </label>
-    </div>
+
+      </Label>
+    </FormFieldWrapper>
   );
 }
 
 FormField.defaultProps = {
   type: 'text',
   value: '',
-  onChange: () => {},
+  onChange: () => { },
 };
 
 FormField.propTypes = {
