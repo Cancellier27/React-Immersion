6c9030e9432cdb4d81a74269ab9adf6a05048137
Cancellier27 <filipecancelliercosta@gmail.com>
2020-08-29 10:57:22 +0100

    creating back end

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,24 +19,41 @@ function Home() {
       .catch((err) => {
         console.log(err.message);
       });
-  });
+  }, []);
 
   return (
-    <PageDefault style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
 
-      {JSON.stringify(dadosIniciais)}
+      {dadosIniciais.length === 0 && <div> Loading...</div>}
 
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-      />
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/* {JSON.stringify(dadosIniciais)} */}
+
+      {/*
 
       <Carousel
         category={dadosIniciais.categorias[1]}
