c229c2151b4447c8661b93ccdc9f0ffc11d648c1
Cancellier27 <filipecancelliercosta@gmail.com>
2020-08-29 11:11:38 +0100

    creating add video page

--- src/pages/cadastro/video/index.jsx
+++ src/pages/cadastro/video/index.jsx
@@ -1,12 +1,40 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/pageDefault';
+import useForm from '../../../hooks/useForm';
+import Button from '../../../components/Menu/Button';
+import FormField from '../../../components/FormFields';
 
 function CadastroVideo() {
+  const history = useHistory();
+  const { handleChange, values } = useForm({
+
+  });
+
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        // eslint-disable-next-line no-alert
+        alert('video cadastrado com sucesso!');
+
+        history.push('/');
+      }}
+      >
+        <FormField
+          label="Título de Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
 
       <Link to="/cadastro/categoria">
         Cadastrar Categoria
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Menu from '../components/Menu';
-// import dadosIniciais from '../data/dados_iniciais.json';
 import BannerMain from '../components/BannerMain';
 import Carousel from '../components/Carousel';
-import Footer from '../components/Footer';
 import categoriasRepository from '../repositories/categorias';
 import PageDefault from '../components/pageDefault';
 
@@ -50,30 +47,6 @@ function Home() {
           />
         );
       })}
-
-      {/* {JSON.stringify(dadosIniciais)} */}
-
-      {/*
-
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      /> */}
 
     </PageDefault>
   );
